c9c19fbc7f35c188dc79c5261e7889c976899914
AlissonCamposMar <100102366+AlissonCamposMar@users.noreply.github.com>
2022-06-01 12:02:00 -0500

    Configuración inicial del proyecto

---
+++ Persistencia/src/main/java/co/edu/uniquindio/biblioteca/PersistenciaApplication.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenciaApplication.class, args);
+    }
+}
